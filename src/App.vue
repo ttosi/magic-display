64f613cffc34b46ea69d0f2e5ce4a7beecf93671
ttosi <ttosi519@gmail.com>
2024-05-13 06:33:11 -0700

    add dim screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup>
-import DateTime from './components/DateTime.vue'
-import Weather from './components/Weather.vue'
-</script>
-
 <template>
   <main class="w-5/12">
     <DateTime />
@@ -11,5 +6,20 @@ import Weather from './components/Weather.vue'
     </div>
   </main>
 </template>
+
+<script setup>
+import DateTime from './components/DateTime.vue'
+import Weather from './components/Weather.vue'
+import dayjs from 'dayjs'
+
+setInterval(() => {
+  document.body.classList.remove('text-white', 'text-slate-400')
+  if (dayjs().hour() > 6 && dayjs().hour() < 7) {
+    document.body.classList.add('text-white')
+  } else {
+    document.body.classList.add('text-slate-400')
+  }
+}, 60000 * 15)
+</script>
 
 <style scoped></style>
