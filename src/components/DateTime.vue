8be0a000b81c4500af1dc4ace4ab37b3c87d2e4c
ttosi <ttosi519@gmail.com>
2025-06-26 14:36:01 -0700

    stuffs

--- src/components/DateTime.vue
+++ src/components/DateTime.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div class="text-8xl font-normal text-center">{{ time }}</div>
+    <div class="text-8xl font-normal text-center w-full">
+      <div v-if="second === 0">{{ timeColon }}</div>
+      <div v-else>{{ timeNoColon }}</div>
+    </div>
     <div class="text-3xl text-center">{{ date }}</div>
   </div>
 </template>
@@ -9,18 +12,22 @@
 import { ref } from 'vue'
 import dayjs from 'dayjs'
 
-const time = ref('')
+const second = ref('')
+const timeColon = ref('')
+const timeNoColon = ref('')
 const date = ref('')
 
 const updateDateTime = () => {
-  time.value = dayjs().format('h:mm A')
+  second.value = Number(dayjs().format('ss')) % 2
+  timeColon.value = dayjs().format('h:mm A')
+  timeNoColon.value = dayjs().format('h mm A')
   date.value = dayjs().format('dddd, MMMM DD')
 }
 
 updateDateTime()
 const dateTimeInterval = setInterval(() => {
   updateDateTime()
-}, 15000)
+}, 1000)
 </script>
 
 <style lang="scss" scoped></style>
